96db2a6b6c0f27d759a41bc4330fcd47718e0d4c
lhyeonnit <lhyeonnit@gmail.com>
2024-05-03 10:04:34 +0900

    시작

--- src/main/java/com/example/store/product/ProductResponse.java
+++ src/main/java/com/example/store/product/ProductResponse.java
@@ -1,38 +1,5 @@
 package com.example.store.product;
 
-import lombok.Data;
-
 public class ProductResponse {
 
-    @Data
-    public static class DetailDTO{
-        private int id;
-        private String name;
-        private int price ;
-        private int qty;
-        private String pic;
-
-        public DetailDTO(Product product){
-            this.id = product.getId();
-            this.name = product.getName();
-            this.price = product.getPrice();
-            this.qty = product.getQty();
-            this.pic = product.getPic();
-        }
-    }
-
-    @Data
-    public static class ListDTO{
-        private int id;
-        private String name;
-        private int price ;
-        private int qty;
-
-        public ListDTO(Product product){
-            this.id = product.getId();
-            this.name = product.getName();
-            this.price = product.getPrice();
-            this.qty = product.getQty();
-        }
-    }
 }
